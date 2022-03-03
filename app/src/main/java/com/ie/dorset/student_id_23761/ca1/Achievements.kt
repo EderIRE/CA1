ce894cae784550121f5d4d256ff56b17db59f119
EderIRE <71097226+EderIRE@users.noreply.github.com>
2022-03-03 13:24:36 +0000

    ✅ Added Material Design layout ✅ Added top row (title bar) ✅ Added bottom row containing three buttons

--- app/src/main/java/com/ie/dorset/student_id_23761/ca1/Achievements.kt
+++ app/src/main/java/com/ie/dorset/student_id_23761/ca1/Achievements.kt
@@ -7,5 +7,7 @@ class Achievements : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.achievements)
+
+
     }
 }
